620cc63756de7c2b288e89954d8c2d4256b264e1
niusmallnan <niusmallnan@gmail.com>
2017-11-06 21:51:10 +0800

    Add api interface

---
+++ service/handlers.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"net/http"
+
+	"github.com/gorilla/context"
+)
+
+func ApiHandler(f http.Handler) http.Handler {
+	return context.ClearHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	}))
+}
+
+func createDomain(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getDomain(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func renewDomain(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateDomain(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteDomain(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ service/routes.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+var routes = Routes{}
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+
+	for _, route := range routes {
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(ApiHandler(route.HandlerFunc))
+	}
+
+	return router
+}
